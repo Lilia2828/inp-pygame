2bb794baf893eb7b5899126d660965221e09d0ae
Olivia <e227804@edu.sbl.ch>
2022-04-05 15:19:40 +0200

    änderungenoli5.04

--- src/main.py
+++ src/main.py
@@ -16,8 +16,8 @@ class Spritesheet:
 
 class Config:
     TILE_SIZE = 32
-    WINDOW_WIDTH = TILE_SIZE * 20
-    WINDOW_HEIGHT = TILE_SIZE * 10
+    WINDOW_WIDTH = TILE_SIZE * 60
+    WINDOW_HEIGHT = TILE_SIZE * 33.75
     BLACK = (0, 0, 0)
     RED = (255, 0, 0)
     GREEN = (0, 255, 0)
@@ -25,7 +25,7 @@ class Config:
     WHITE = (255, 255, 255)
     FPS = 30
     MAX_GRAVITY = -3
-    BG_SPEED = 0.3
+    BG_SPEED = 0
 
 
 
@@ -67,7 +67,8 @@ class PlayerSprite(BaseSprite):
             'spritesheet': Spritesheet("res/player.png"),
         }
         super().__init__(game, x, y, groups=game.players, layer=1, **img_data, **kwargs)
-        self.y_velocity = Config.MAX_GRAVITY
+        self.y_velocity = 0
+        self.x_velocity = 0
         self.speed = 5
         self.standing = False
         self.color = Config.RED
@@ -91,7 +92,7 @@ class PlayerSprite(BaseSprite):
     def update(self):
         self.handle_movement()
         self.rect.y = self.rect.y - self.y_velocity
-        self.rect.x = self.rect.x - self.y_velocity
+        self.rect.x = self.rect.x - self.x_velocity
 
         self.check_collision()
         self.y_velocity = max(self.y_velocity - 0.5, Config.MAX_GRAVITY)
@@ -99,6 +100,7 @@ class PlayerSprite(BaseSprite):
     def jump(self):
         if self.standing:
             self.y_velocity = self.jump_force
+            self.x_velocity = 5
             self.standing = False
 
     def handle_movement(self):
@@ -154,6 +156,8 @@ class PlayerSprite(BaseSprite):
 
     def check_collision(self):
         hits = pygame.sprite.spritecollide(self, self.game.ground, False)
+        if hits: 
+            self.x_velocity = 0
         for hit in hits:
             if self.is_standing(hit):
                 self.rect.bottom = hit.rect.top
@@ -182,6 +186,15 @@ class GroundSprite(BaseSprite):
         super().__init__(game, x, y, groups=game.ground, layer=0)
         self.image.fill(Config.GREEN)
 
+class KrokodileSprite(BaseSprite):
+    def __init__(self, game, x, y):
+        img_data = {
+            "spritesheet": Spritesheet("res/kroko.png"),
+            "width": 666,
+            "height": 512,
+        }
+        super().__init__(game, x, y, groups=game.kroko, layer=1, **img_data)
+        
 
 class Game:
     def __init__(self):
@@ -190,7 +203,7 @@ class Game:
         self.font = pygame.font.Font(None, 30)
         self.screen = pygame.display.set_mode( (Config.WINDOW_WIDTH, Config.WINDOW_HEIGHT) ) 
         self.clock = pygame.time.Clock()
-        self.bg = pygame.image.load("res/neu.png")
+        self.bg = pygame.image.load("res/Hintergrund ohne krokodil.png")
         self.bg_x = 0
 
     
@@ -202,6 +215,8 @@ class Game:
                         GroundSprite(self, x, y)
                     if c == "p":
                         self.player = PlayerSprite(self, x, y)
+                    if c == "k":
+                        KrokodileSprite(self, x, y)
 
     def new(self):
         self.playing = True
@@ -209,6 +224,7 @@ class Game:
         self.all_sprites = pygame.sprite.LayeredUpdates()
         self.ground = pygame.sprite.LayeredUpdates()
         self.players = pygame.sprite.LayeredUpdates()
+        self.kroko = pygame.sprite.LayeredUpdates()
 
         self.load_map("maps/level-01.txt")
 
