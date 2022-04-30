41a056c76cb1eaf75ce9fa30883f9bb72f12901f
Olivia <e227804@edu.sbl.ch>
2022-04-30 15:31:03 +0200

    welcome ergänzt

--- src/main.py
+++ src/main.py
@@ -305,18 +305,17 @@ class Game:
         counter = 0
             
         while True:
-            self.screen.fill(Config.RED)
-            display_text = self.font.render('Press Space to Start...', False, (0, 0, 0))
+            self.screen.fill(Config.GREEN)
+            display_text = self.font.render('Klicke auf c um "Feed the croco" zu starten ', False, (0, 0, 0))
             self.screen.blit(display_text, (200, 50))
-            counter_text = self.font.render(f'{counter}', False, (0, 0, 0))
-            self.screen.blit(counter_text, (200, 100))
+    
             pygame.display.flip()
             self.clock.tick(Config.FPS)
-            counter += 1
+            
 
             pygame.event.get()
             keys = pygame.key.get_pressed()
-            if keys[pygame.K_SPACE]:
+            if keys[pygame.K_c]:
                 break
 
 def load_and_scale_img(img_path, size):
